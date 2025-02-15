a02fedbb6587e97d73c4e75bbde4dec6c3d0ce18
Gleidson De Sousa <desousag2000@gmail.com>
2025-02-15 02:36:30 -0500

    Complete module 8 minus the challenge

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
 from django.views import generic
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
@@ -59,6 +61,19 @@ class BookListView(generic.ListView):
 
 class BookDetailView(generic.DetailView):
   model = Book
+
+class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
+  """Generic class-based view listing books on loan to current user."""
+  model = BookInstance
+  template_name = 'catalog/bookinstance_list_borrowed_user.html'
+  paginate_by = 10
+
+  def get_queryset(self):
+    return (
+      BookInstance.objects.filter(borrower=self.request.user)
+      .filter(status__exact='o')
+      .order_by('due_back')
+    )
 
 class AuthorListView(generic.ListView):
   """Generic class-based list view for a list of authors."""
